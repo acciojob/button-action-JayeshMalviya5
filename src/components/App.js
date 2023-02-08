318a575ad7613cb50d62addc9074b91f52ef7953
jayeshmalviya5 <jayeshmalviya10@gmail.com>
2023-02-08 23:22:19 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,14 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [renderClick, setState] = useState(false);
+  const renderIt = ()=>{
+    setState(true);
+  }
   return (
     <div id="main">
-      // Do not alter the main div
+     {renderClick ? <p>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p> : null }
+      <button id="click" onClick={renderIt}>Click me</button>
     </div>
   );
 }
